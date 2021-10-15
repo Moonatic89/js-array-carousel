b87f3b1010cbaaef1331fd285a481f0dbfe4bb18
StefanoNesi <sn.boolean@gmail.com>
2021-10-15 19:00:40 +0200

    Technically completed, listUpdate is to be remaded completely

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,4 +1,5 @@
-//Array
+
+//#region Base Array
 
 const items = [
     'img/01.jpg',
@@ -23,6 +24,9 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+
+//#endregion
+
 //#region Init Variables
 
 //buttons
@@ -35,12 +39,11 @@ let showcaseImage = document.querySelector(".showcase>.imgContainer");
 let showcaseTitle = document.querySelector(".showcaseDesc>h1");
 let showcaseText = document.querySelector(".showcaseDesc>.text");
 
-console.log(showcaseImage);
 //#endregion
 
 //Add event listeners
-btnPlus.addEventListener("click", indexPlus); ///Change function
-btnMinus.addEventListener("click", indexMinus); ///Change function
+btnPlus.addEventListener("click", indexMinus); ///Change function
+btnMinus.addEventListener("click", indexPlus); ///Change function
 
 //Function indexPlus
 
@@ -49,40 +52,70 @@ function indexPlus() {
     //Increment index by 1
     carouselIndex += 1;
 
-    //BONUS check if index is > 4 
-    if (carouselIndex > 4) {
-        carouselIndex = 0
-    }
-
-    showcaseUpdate();
-
-    debugConsoleLog(carouselIndex);
+    //This recalls the main update function, that updates everything
+    mainUpdate();
 }
 
 function indexMinus() {
+
     //Increment index by 1
     carouselIndex -= 1;
 
-    //BONUS check if index is > 4 
-    if (carouselIndex < 0) {
+    //This recalls the main update function, that updates everything
+    mainUpdate();
+}
+
+function mainUpdate() {
+
+    indexControl();
+    showcaseUpdate();
+    listUpdate();
+}
+
+
+//BONUS endless cycle
+function indexControl() {
+
+    if (carouselIndex > 4) {
+        carouselIndex = 0
+
+    } else if (carouselIndex < 0) {
         carouselIndex = 4
     }
-
-    showcaseUpdate();
-
-    debugConsoleLog(carouselIndex);
 }
 
 function showcaseUpdate() {
     showcaseImage.innerHTML = `<img src="./asset/${items[carouselIndex]}" alt=""></img>`;
-    showcaseTitle
-    showcaseText
+    showcaseTitle.innerHTML = title[carouselIndex];
+    showcaseText.innerHTML = text[carouselIndex];
+}
+
+function listUpdate() {
+    let listItem0 = document.getElementById("item0");
+    let listItem1 = document.getElementById("item1");
+    let listItem2 = document.getElementById("item2");
+    let listItem3 = document.getElementById("item3");
+    let listItem4 = document.getElementById("item4");
+    let listItem = document.getElementById(`item${carouselIndex}`);
+
+    listItem0.classList.add("overlay");
+    listItem0.classList.remove("customBord");
+    listItem1.classList.add("overlay");
+    listItem1.classList.remove("customBord");
+    listItem2.classList.add("overlay");
+    listItem2.classList.remove("customBord");
+    listItem3.classList.add("overlay");
+    listItem3.classList.remove("customBord");
+    listItem4.classList.add("overlay");
+    listItem4.classList.remove("customBord");
+
+    listItem.classList.add("customBord");
+    listItem.classList.remove("overlay");
 }
 
 
 
-
-//This is only for debug purposes
+//This is only for debug purposes, that's why i didnt used a void function
 function debugConsoleLog(intToCheck) {
     console.log(intToCheck);
 }
