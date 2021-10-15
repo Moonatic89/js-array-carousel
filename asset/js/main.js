30769211e76c8121b6621f209a660395918693f3
StefanoNesi <sn.boolean@gmail.com>
2021-10-15 18:25:22 +0200

    carousel working, only list overlay missing

--- asset/js/main.js
+++ asset/js/main.js
@@ -23,11 +23,21 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+//#region Init Variables
 
-// Create button
+//buttons
 const btnPlus = document.getElementById("plus");
 const btnMinus = document.getElementById("minus");
-let carouselIndex = 1;
+let carouselIndex = 0;
+
+//showcase
+let showcaseImage = document.querySelector(".showcase>.imgContainer");
+let showcaseTitle = document.querySelector(".showcaseDesc>h1");
+let showcaseText = document.querySelector(".showcaseDesc>.text");
+
+console.log(showcaseImage);
+//#endregion
+
 //Add event listeners
 btnPlus.addEventListener("click", indexPlus); ///Change function
 btnMinus.addEventListener("click", indexMinus); ///Change function
@@ -36,8 +46,43 @@ btnMinus.addEventListener("click", indexMinus); ///Change function
 
 function indexPlus() {
 
+    //Increment index by 1
+    carouselIndex += 1;
+
+    //BONUS check if index is > 4 
+    if (carouselIndex > 4) {
+        carouselIndex = 0
+    }
+
+    showcaseUpdate();
+
+    debugConsoleLog(carouselIndex);
 }
 
 function indexMinus() {
+    //Increment index by 1
+    carouselIndex -= 1;
 
+    //BONUS check if index is > 4 
+    if (carouselIndex < 0) {
+        carouselIndex = 4
+    }
+
+    showcaseUpdate();
+
+    debugConsoleLog(carouselIndex);
+}
+
+function showcaseUpdate() {
+    showcaseImage.innerHTML = `<img src="./asset/${items[carouselIndex]}" alt=""></img>`;
+    showcaseTitle
+    showcaseText
+}
+
+
+
+
+//This is only for debug purposes
+function debugConsoleLog(intToCheck) {
+    console.log(intToCheck);
 }
